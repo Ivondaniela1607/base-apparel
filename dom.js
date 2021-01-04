49845a7bade4f5ee6477eeb9d37aa0ce0904c43f
IvonneDaniela <ivondaniela1607@gmail.com>
2021-01-04 10:47:32 -0500

    change icon excla

---
+++ dom.js
@@ -0,0 +1,6 @@
+function view(){
+  document.getElementById('msj').style.display = "block";
+  document.getElementById('input').style.border = "thin solid hsl(0, 93%, 68%)";
+  document.getElementById('iconExcl').style.display = "block";
+}
+
